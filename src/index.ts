2cb7c1dd0739c2326f65dd2c50a36b374a9c3909
Aleksandar Avramov <aleksandar.avramov@shelly.com>
2026-07-05 11:10:58 +0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Express } from 'express';
+import express, { Express, json } from 'express';
 import { Main } from './app/main';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,16 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.use(helmet());
+app.use(json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+);
 
 new Main(app).bootstrap();
 
